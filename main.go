3d924a88c7332d457cacc2e6f4af6dace646d7c9
IlyaOsotov <soten58@yandex.ru>
2019-08-25 12:48:55 +0300

    pointeeers

--- main.go
+++ main.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
 func main() {
-	fmt.Println("Structs in Go")
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "test@test.test",
+			zipCode: 123123,
+		},
+	}
+
+	(&alex).updateFirstName("Ilya")
+
+	alex.print()
+}
+
+func (pointerToPerson *person) updateFirstName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
